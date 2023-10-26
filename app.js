5d1e716c0fbdc0577e057d9d16849c4470f02087
Ido <Idosabach11@gmail.com>
2023-10-26 16:31:44 +0300

    add JavaScript

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // INTERFACE
-
 const add = '+';
 const subtract = '-';
 const multiply = '*';
 const divide = '/';
+const arr = [];
 
 let num1 , num2;
 
@@ -20,7 +20,7 @@ function divideFun(num1,num2){
   return num1 / num2;
 }
 
-function allCalculate(operator,num1,num2){
+function allCalculate(num1,operator,num2){
   if(operator===add){
     return addFun(num1,num2)
   }else if(operator===subtract){
@@ -34,12 +34,45 @@ function allCalculate(operator,num1,num2){
 
 // UI
 
-const clearAll = document.querySelector('.clearAll');
-const deleteOne = document.querySelector('.delete');
-const operator = document.querySelectorAll('[data-operator]')
-const number = document.querySelectorAll('[data-num]')
+const calculate = document.querySelector('.calculate')
+const result = document.querySelector('.result')
+const clearAll = document.querySelector('.clearAll')
+const deleteOne = document.querySelector('.delete')
+const operator = document.querySelectorAll('#operator')
+const numbers = document.querySelectorAll('#number')
 const point = document.querySelector('.point')
 const equal = document.querySelector('.equal')
+const one = document.querySelector('.one')
+
 
 clearAll.addEventListener('click',clear)
 deleteOne.addEventListener('click',remove)
+
+numbers.forEach(button=>{
+  button.addEventListener('click',()=>{
+    const number = button.textContent;
+    calculate.textContent += `${number}`
+    addToArr(number)
+    console.log(arr);
+  })
+})
+
+operator.forEach(button=>{
+  button.addEventListener('click',()=>{
+    const operator = button.textContent;
+    calculate.textContent += `${operator}`
+    console.log(operator);
+  })
+})
+
+function clear(){
+  calculate.textContent = '';
+  result.textContent = '';
+}
+
+
+
+
+
+
+
