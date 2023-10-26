81157a3de6ee6a78ea8adff9bee7d900ecec396c
Ido <Idosabach11@gmail.com>
2023-10-26 03:25:43 +0300

    app(add): add UI to app.js

--- app.js
+++ app.js
@@ -31,3 +31,15 @@ function allCalculate(operator,num1,num2){
     return divideFun(num1,num2)
   }
 }
+
+// UI
+
+const clearAll = document.querySelector('.clearAll');
+const deleteOne = document.querySelector('.delete');
+const operator = document.querySelectorAll('[data-operator]')
+const number = document.querySelectorAll('[data-num]')
+const point = document.querySelector('.point')
+const equal = document.querySelector('.equal')
+
+clearAll.addEventListener('click',clear)
+deleteOne.addEventListener('click',remove)
