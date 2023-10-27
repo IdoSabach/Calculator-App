a6bd0b4707c1c8644552f63817d36b799b6db7d1
Ido <Idosabach11@gmail.com>
2023-10-27 13:46:22 +0300

    add more function

--- app.js
+++ app.js
@@ -17,15 +17,20 @@ function multipleFun(num1,num2){
   return num1 * num2;
 }
 function divideFun(num1,num2){
+  if(num1===0 || num2===0 || (num1===0 && num2===0)){
+    return "Error"
+  }
   return num1 / num2;
 }
+
+
 
 function allCalculate(num1,operator,num2){
   if(operator===add){
     return addFun(num1,num2)
   }else if(operator===subtract){
     return subtractFun(num1,num2)
-  }else if(operator===multiple){
+  }else if(operator===multiply){
     return multipleFun(num1,num2)
   }else{
     return divideFun(num1,num2)
@@ -38,31 +43,41 @@ const calculate = document.querySelector('.calculate')
 const result = document.querySelector('.result')
 const clearAll = document.querySelector('.clearAll')
 const deleteOne = document.querySelector('.delete')
-const operator = document.querySelectorAll('#operator')
-const numbers = document.querySelectorAll('#number')
+const operator = document.querySelectorAll('.btn-operator')
+const numbers = document.querySelectorAll('.btn-number')
 const point = document.querySelector('.point')
 const equal = document.querySelector('.equal')
-const one = document.querySelector('.one')
+
+
 
 
 clearAll.addEventListener('click',clear)
 deleteOne.addEventListener('click',remove)
 
-numbers.forEach(button=>{
-  button.addEventListener('click',()=>{
-    const number = button.textContent;
-    calculate.textContent += `${number}`
-    // console.log(arr);
+numbers.forEach((button)=>{
+  button.addEventListener('click',function(){
+    const num = button.textContent
+    getNumber(num);
+  })
+})
+operator.forEach((button)=>{
+  button.addEventListener('click',function(){
+    const opera = button.textContent
+    getOperator(opera);
   })
 })
 
-operator.forEach(button=>{
-  button.addEventListener('click',()=>{
-    const operator = button.textContent;
-    calculate.textContent += `${operator}`
-    // console.log(operator);
-  })
-})
+equal.addEventListener('click',equalCalculate)
+
+
+
+function updateDisplayResult(value){
+ result.textContent += value;
+}
+
+function updateDisplayCalculate(value){
+  calculate.textContent += value;
+ }
 
 function clear(){
   calculate.textContent = '';
@@ -70,8 +85,25 @@ function clear(){
 }
 
 function remove(){
+  let allText = calculate.textContent
+  if(allText>0){
+    allText = allText.slice(0,-1);
+    calculate.textContent = allText;
+  }
+}
+
+function getNumber(number){
+  updateDisplayCalculate(number);
+}
+
+function getOperator(operator){
+  updateDisplayCalculate(operator);
+}
+
+function equalCalculate(){
   return
 }
+
 
 
 
