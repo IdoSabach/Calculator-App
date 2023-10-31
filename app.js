f95b1dbf612ad3aa7ecee98caa89891d0970ee55
Ido <Idosabach11@gmail.com>
2023-10-31 13:05:25 +0200

    fix format

--- app.js
+++ app.js
@@ -1,124 +1,113 @@
-
-const ADD = '+';
-const SUBTRACT = '-';
-const MULTIPLY = '*';
-const DIVIDE = '/';
-let num1 =''
-let num2 =''
+const ADD = "+";
+const SUBTRACT = "-";
+const MULTIPLY = "*";
+const DIVIDE = "/";
+let num1 = "";
+let num2 = "";
 let op = null;
-let removeAllScreen = false
+let removeAllScreen = false;
 
 // INTERFACE
 
-function addFun(numOne,numTwo){
+function addFun(numOne, numTwo) {
   return numOne + numTwo;
 }
-function subtractFun(numOne,numTwo){
+function subtractFun(numOne, numTwo) {
   return numOne - numTwo;
 }
-function multiplyFun(numOne,numTwo){
+function multiplyFun(numOne, numTwo) {
   return numOne * numTwo;
 }
-function divideFun(numOne,numTwo){
-  if(numOne===0 || numTwo===0 || (numOne===0 && numTwo===0)){
-    return "Error"
+function divideFun(numOne, numTwo) {
+  if (numOne === 0 || numTwo === 0 || (numOne === 0 && numTwo === 0)) {
+    return "Error";
   }
   return numOne / numTwo;
 }
 
-
-function allCalculate(numOne,operator,numTwo){
-  numOne =Number(numOne)
-  numTwo =Number(numTwo)
-  if(operator===ADD){
-    return addFun(numOne,numTwo)
-  }else if(operator===SUBTRACT){
-    return subtractFun(numOne,numTwo)
-  }else if(operator===MULTIPLY){
-    return multiplyFun(numOne,numTwo)
-  }else if(operator===DIVIDE){
-    return divideFun(numOne,numTwo)
+function allCalculate(numOne, operator, numTwo) {
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
+  if (operator === ADD) {
+    return addFun(numOne, numTwo);
+  } else if (operator === SUBTRACT) {
+    return subtractFun(numOne, numTwo);
+  } else if (operator === MULTIPLY) {
+    return multiplyFun(numOne, numTwo);
+  } else if (operator === DIVIDE) {
+    return divideFun(numOne, numTwo);
   }
 }
 
-const calculate = document.querySelector('.calculate')
-const result = document.querySelector('.result')
-const clearAll = document.querySelector('.clearAll')
-const deleteOne = document.querySelector('.delete')
-const percent = document.querySelector('.percent')
-const operator = document.querySelectorAll('.btn-operator')
-const numbers = document.querySelectorAll('.btn-number')
-const point = document.querySelector('.point')
-const equal = document.querySelector('.equal')
+const calculate = document.querySelector(".calculate");
+const result = document.querySelector(".result");
+const clearAll = document.querySelector(".clearAll");
+const deleteOne = document.querySelector(".delete");
+const percent = document.querySelector(".percent");
+const operator = document.querySelectorAll(".btn-operator");
+const numbers = document.querySelectorAll(".btn-number");
+const point = document.querySelector(".point");
+const equal = document.querySelector(".equal");
 
-clearAll.addEventListener('click',clearAllFunc)
-deleteOne.addEventListener('click',removeOne)
-equal.addEventListener('click',calculateResult)
-point.addEventListener('click',addPoint)
+clearAll.addEventListener("click", clearAllFunc);
+deleteOne.addEventListener("click", removeOne);
+equal.addEventListener("click", calculateResult);
+point.addEventListener("click", addPoint);
 
-function addPoint(){
-  if(result.textContent===""){
-    result.textContent +='0'
+function addPoint() {
+  if (result.textContent === "") {
+    result.textContent += "0";
   }
-  if(result.textContent.includes('.')){
-    return
+  if (result.textContent.includes(".")) {
+    return;
   }
-  result.textContent +='.'
+  result.textContent += ".";
 }
 
-operator.forEach((button)=>{
-  button.addEventListener('click',()=> getOperator(button.textContent))
-})
+operator.forEach((button) => {
+  button.addEventListener("click", () => getOperator(button.textContent));
+});
 
-numbers.forEach((button)=>{
-  button.addEventListener('click',()=> getNumber(button.textContent))
-})
+numbers.forEach((button) => {
+  button.addEventListener("click", () => getNumber(button.textContent));
+});
 
-
-
-
-
-function getOperator(operator){
-  if(op!==null){
-    calculateResult()
+function getOperator(operator) {
+  if (op !== null) {
+    calculateResult();
   }
-  num1 = result.textContent
-  op = operator
-  calculate.textContent = `${num1} ${op}`
-  removeAllScreen = true
+  num1 = result.textContent;
+  op = operator;
+  calculate.textContent = `${num1} ${op}`;
+  removeAllScreen = true;
 }
 
-
-function getNumber(num){
-  if(removeAllScreen){
-    result.textContent = ""
-    removeAllScreen = false
+function getNumber(num) {
+  if (removeAllScreen) {
+    result.textContent = "";
+    removeAllScreen = false;
   }
-  result.textContent += num
+  result.textContent += num;
 }
 
-
-function calculateResult(){
-  if(op===null || removeAllScreen){
-    return
+function calculateResult() {
+  if (op === null || removeAllScreen) {
+    return;
   }
-  num2 = result.textContent
-  result.textContent = allCalculate(num1,op,num2)
-  calculate.textContent = `${num1} ${op} ${num2} =`
-  op = null
+  num2 = result.textContent;
+  result.textContent = allCalculate(num1, op, num2);
+  calculate.textContent = `${num1} ${op} ${num2} =`;
+  op = null;
 }
 
-
-function removeOne(){
-  calculate.textContent = calculate.textContent.slice(0,-1)
+function removeOne() {
+  calculate.textContent = calculate.textContent.slice(0, -1);
 }
 
-
-
-function clearAllFunc(){
-  calculate.textContent = ""
-  result.textContent = ""
-  num1 = ''
-  num2 =''
-  op = null
+function clearAllFunc() {
+  calculate.textContent = "";
+  result.textContent = "";
+  num1 = "";
+  num2 = "";
+  op = null;
 }
